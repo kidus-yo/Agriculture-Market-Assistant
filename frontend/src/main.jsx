9692e3d2bb22519b450cc4a5da273334509ef660
eyob-tk <293199826+eyob-tk@users.noreply.github.com>
2026-09-16 23:11:27 +0300

    welcome page edited

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,18 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './assets/css-files/index.css'
 import './assets/css-files/background.css'
 import './assets/css-files/welcome.css'
+import './assets/css-files/register-login.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <div className="aura-bg">
-        <div className="aura-layer-1" />
-        <div className="aura-layer-2" />
-        <div className="aura-content">
-          <App />
-        </div>
-      </div>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
